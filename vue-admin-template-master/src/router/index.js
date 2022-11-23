aa27f1a84a42f640d54087d9d6b2724432bd83f6
renhai1 <1457481392@qq.com>
2022-11-23 09:27:26 +0800

    部门管理-莫力源

--- vue-admin-template-master/src/router/index.js
+++ vue-admin-template-master/src/router/index.js
@@ -21,11 +21,18 @@ import Layout from '../views/layout/Layout'
     icon: 'svg-name'             the icon show in the sidebar,
   }
 **/
-export const constantRouterMap = [
-    { path: '/login', component: () =>
-            import ('@/views/login/index'), hidden: true },
-    { path: '/404', component: () =>
-            import ('@/views/404'), hidden: true },
+export const constantRouterMap = [{
+        path: '/login',
+        component: () =>
+            import ('@/views/login/index'),
+        hidden: true
+    },
+    {
+        path: '/404',
+        component: () =>
+            import ('@/views/404'),
+        hidden: true
+    },
 
     {
         path: '/',
